efcad1c1dda2fe6608afc30fd3673a6c2fe31a11
arthyssj <emiliofigueroa661@gmail.com>
2026-04-08 20:01:12 -0500

    Primer commit: nivel 1

--- Jugador.cs
+++ Jugador.cs
@@ -15,6 +15,7 @@ namespace JuegoLaberinto
         }
         public void Dibujar(Graphics g)
         {
+            //Color del rectangulo
             g.FillRectangle(Brushes.Red, Area());
         }
         public void Mover(string direccion)
